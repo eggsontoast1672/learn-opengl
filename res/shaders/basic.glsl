b764aa9dd0e80eacee3b9f9a56d10df97f867010
Paul Zupan <paul.zupan.13@gmail.com>
2022-02-27 23:31:00 -0800

    Rectangle changes colors now

--- res/shaders/basic.glsl
+++ res/shaders/basic.glsl
@@ -17,6 +17,8 @@ void main() {
 
 layout(location = 0) out vec4 color;
 
+uniform vec4 u_color;
+
 void main() {
-    color = vec4(0.0f, 0.6f, 1.0f, 1.0f);
+    color = u_color;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,37 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define SNBL_ASSERT(x) \
+    if (!(x)) \
+        raise(SIGTRAP)
+
+#define SNBL_CALL(x) \
+    snbl_clearError(); \
+    x; \
+    SNBL_ASSERT(snbl_logCall(__FILE__, __LINE__, #x))
+
 struct ShaderProgramSource {
     std::string vertex;
     std::string fragment;
 };
 
-static ShaderProgramSource ParseShader(const std::string& file) {
+static void snbl_clearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool snbl_logCall(const char *file, int line, const char *function) {
+    GLenum error;
+    while (error = glGetError()) {
+        std::cout << "File: " << file << '\n'
+                  << "Line: " << line << '\n'
+                  << "Function: " << function << '\n'
+                  << "Error: " << error << std::endl;
+        return false;
+    }
+    return true;
+}
+
+static ShaderProgramSource snbl_parseShader(const std::string& file) {
     enum class ShaderType {
         NONE = -1,
         VERTEX = 0,
@@ -41,27 +66,27 @@ static ShaderProgramSource ParseShader(const std::string& file) {
     return { ss[0].str(), ss[1].str() };
 }
 
-static unsigned int CompileShader(unsigned int type, const std::string& source) {
-    unsigned int id = glCreateShader(type);
+static unsigned int snbl_compileShader(unsigned int type, const std::string& source) {
+    SNBL_CALL(unsigned int id = glCreateShader(type));
     const char *src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
+    SNBL_CALL(glShaderSource(id, 1, &src, nullptr));
+    SNBL_CALL(glCompileShader(id));
 
     // TODO: Handle shader compilation errors
     int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    SNBL_CALL(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
     if (result == GL_FALSE) {
         int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        SNBL_CALL(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
         char message[length];
-        glGetShaderInfoLog(id, length, &length, message);
+        SNBL_CALL(glGetShaderInfoLog(id, length, &length, message));
 
         const char *typeFmt;
         if (type == GL_VERTEX_SHADER) typeFmt = "vertex";
         else if (type == GL_FRAGMENT_SHADER) typeFmt = "fragment";
         std::cout << "Failed to compile " << typeFmt << " shader: " << message << std::endl;
 
-        glDeleteShader(id);
+        SNBL_CALL(glDeleteShader(id));
 
         return 0;
     }
@@ -69,18 +94,18 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
     return id;
 }
 
-static unsigned int CreateShader(const std::string& vertexSource, const std::string& fragmentSource) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexSource);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentSource);
+static unsigned int snbl_createShader(const std::string& vertexSource, const std::string& fragmentSource) {
+    SNBL_CALL(unsigned int program = glCreateProgram());
+    SNBL_CALL(unsigned int vs = snbl_compileShader(GL_VERTEX_SHADER, vertexSource));
+    SNBL_CALL(unsigned int fs = snbl_compileShader(GL_FRAGMENT_SHADER, fragmentSource));
 
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
+    SNBL_CALL(glAttachShader(program, vs));
+    SNBL_CALL(glAttachShader(program, fs));
+    SNBL_CALL(glLinkProgram(program));
+    SNBL_CALL(glValidateProgram(program));
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    SNBL_CALL(glDeleteShader(vs));
+    SNBL_CALL(glDeleteShader(fs));
 
     return program;
 }
@@ -97,6 +122,7 @@ int main(void) {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     if (glewInit() != GLEW_OK) {
         glfwTerminate();
@@ -131,29 +157,42 @@ int main(void) {
         "color = vec4(1.0f, 0.0f, 0.0f, 1.0f);"
         "}";
 
-    ShaderProgramSource source = ParseShader("../res/shaders/basic.shader");
-    unsigned int shader = CreateShader(source.vertex, source.fragment);
-    glUseProgram(shader);
-
     /* Create a vertex buffer and put data inside it */
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW);
+    SNBL_CALL(glGenBuffers(1, &buffer));
+    SNBL_CALL(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    SNBL_CALL(glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    SNBL_CALL(glEnableVertexAttribArray(0));
+    SNBL_CALL(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
 
     unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    SNBL_CALL(glGenBuffers(1, &ibo));
+    SNBL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    SNBL_CALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+
+    ShaderProgramSource source = snbl_parseShader("../res/shaders/basic.glsl");
+    unsigned int shader = snbl_createShader(source.vertex, source.fragment);
+    SNBL_CALL(glUseProgram(shader));
+
+    SNBL_CALL(int location = glGetUniformLocation(shader, "u_color"));
+    SNBL_ASSERT(location != -1);
+
+    float r = 0.0f;
+    float increment = 0.05f;
 
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        SNBL_CALL(glClear(GL_COLOR_BUFFER_BIT));
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        /* Draw the square */
+        SNBL_CALL(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+        SNBL_CALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        /* Animate the color of the rectangle */
+        if (r > 1.0f || r < 0.0f)
+            increment *= -1;
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -162,10 +201,8 @@ int main(void) {
         glfwPollEvents();
     }
 
-    glDeleteProgram(shader);
+    SNBL_CALL(glDeleteProgram(shader));
     glfwTerminate();
-
-    raise(SIGTRAP);
 
     return 0;
 }
